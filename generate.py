9d6be18d4ec88924443b4ac976f8532fd7b1b1de
Nickwasused <1771@gmx.de>
2019-01-27 11:05:38 +0100

    added new files
    better randomness

--- generate.py
+++ generate.py
@@ -17,13 +17,19 @@ print('Got {} Names!'.format(num_lines))
 
 chars = string.ascii_letters + string.digits + '!@#$%^&*()'
 random.seed = (os.urandom(1024))
-email_domains = ['gmail.com', 'yahoo.com', 'hotmail.com', 'aol.com', 'outlook.com', 'gmx.de', 'gmx.net',
-                 'protonmail.com', 'fastmail.com', 'zoho.com',
-                 'mail.com', 'hushmail.com']
+email_domains = ['gmail.', 'yahoo.', 'hotmail.', 'aol.', 'outlook.', 'gmx.', 'Yandex.', 'icloud', 'tutanota',
+                 'protonmail.', 'fastmail.', 'zoho.', 'mail.', 'hushmail.', 'aichi.', 'aim.', 'airforce.', 'airforceemail.', 'airmail.', 'airpost.']
+
+domain_end = ['com', 'org', 'net', 'int', 'edu', 'gov', 'ac', 'ad', 'ae', 'af', 'ag', 'ai', 'al', 'am', 'an', 'ao', 'aq', 'ar', 'as', 'at', 'au', 'aw', 'ax', 'az', 'ba', 'bb', 'bd',
+              'be', 'bf', 'bg', 'bh', 'bi', 'bj', 'bl', 'bm', 'bn', 'bn', 'bm', 'bo', 'bq', 'br', 'bs', 'bt', 'bw', 'by', 'bz', 'ca', 'cc', 'cd', 'cf', 'cg', 'ch', 'ci', 'ck', 'cl',
+              'cm', 'cn', 'co', 'cr', 'cu', 'cv', 'cw', 'cx', 'cy', 'cz', 'fi', 'fj', 'fk', 'fm', 'fo', 'fr', 'ga', 'gd', 'ge', 'gf', 'gg', 'gh', 'hk', 'hm', 'hn', 'hr', 'ht', 'hu',
+              'de', 'dj', 'dk', 'dm', 'do', 'dz', 'ec', 'ee', 'eg', 'er', 'es', 'et', 'eu', 'id', 'ie', 'il', 'im', 'in', 'io', 'iq', 'is', 'it', 'jm', 'jo', 'jp', 'ke', 'kg', 'kh',
+              'ki', 'km', 'kn', 'kp', 'kr', 'li', 'ma', 'mc', 'md', 'mh', 'nl', 'no', 'np', 'nz', 'om', 'pa', 'pl', 'rs', 'ru', 'sa', 'sd', 'se', 'sg', 'si', 'sk', 'tc', 'td', 'tr',
+              'tw', 'ua', 'ug', 'uk', 'um', 'us', 'uz', 'uy', 'vn', 'vu', 'za', 'zm', 'zw', 'mail']
 
 names = json.loads(open('names.json', encoding="utf8").read())
 surnames = json.loads(open('names.json', encoding="utf8").read())
-words = json.loads(open('words.json', encoding="utf8").read())
+passwordlist = json.loads(open('passlist.json', encoding="utf8").read())
 
 kek = 1
 
@@ -33,11 +39,9 @@ def scam():
         while True:
             name = random.choice(names).lower() + random.choice(surnames).lower()
             name_extra = ''.join(random.choice(string.digits) for i in range(random.randint(1, 4)))
-            email_domain = random.choice(email_domains)
-            word = random.choice(words)
-            word_extra = ''.join(random.choice(chars) for i in range(random.randint(1, 4)))
+            email_domain = random.choice(email_domains) + random.choice(domain_end)
             email = name + name_extra + '@' + email_domain
-            password = word_extra + word + word_extra
+            password = random.choice(passwordlist)
             combo = ('{0}:{1} \n'.format(email, password)).encode()
             f.write(combo)
             global kek
