9a8e212adbc2faead99b2a9f5e74da83c0a4018a
Nickwasused <1771@gmx.de>
2019-02-15 19:01:45 +0100

    Add files via upload

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@ import os
 import random
 import string
 import json
-import multiprocessing
+import concurrent.futures
 from colorama import init
 from colorama import Fore, Style
 
@@ -31,168 +31,21 @@ names = json.loads(open('names.json', encoding="utf8").read())
 surnames = json.loads(open('names.json', encoding="utf8").read())
 passwordlist = json.loads(open('passlist.json', encoding="utf8").read())
 
-kek = 1
+def generate():
+    name = random.choice(names).lower() + random.choice(surnames).lower()
+    name_extra = ''.join(random.choice(string.digits) for i in range(random.randint(1, 4)))
+    email_domain = random.choice(email_domains) + random.choice(domain_end)
+    email = name + name_extra + '@' + email_domain
+    password = random.choice(passwordlist)
+    combo = ('{0}:{1}'.format(email, password)).encode()
+    return combo
 
-
-def scam():
-    with open(file, 'wb') as f:
-        while True:
-            name = random.choice(names).lower() + random.choice(surnames).lower()
-            name_extra = ''.join(random.choice(string.digits) for i in range(random.randint(1, 4)))
-            email_domain = random.choice(email_domains) + random.choice(domain_end)
-            email = name + name_extra + '@' + email_domain
-            password = random.choice(passwordlist)
-            combo = ('{0}:{1} \n'.format(email, password)).encode()
+def gen():
+    with open(file, 'ab') as f:
+            combo = generate() + '\n'.encode()
             f.write(combo)
-            global kek
-            kek = (kek + 1)
-            lel = random.randint(1, 2)
-            if lel == 1:
-                print(Fore.RED + '{}        {}'.format(kek, combo))
-            if lel == 2:
-                print(Fore.CYAN + '{}       {}'.format(kek, combo))
-            pass
+            f.close()
 
-
-if __name__ == '__main__':
-    worker_0 = multiprocessing.Process(
-        name='worker 0',
-        target=scam(),
-    )
-    worker_1 = multiprocessing.Process(
-        name='worker 1',
-        target=scam(),
-    )
-    worker_2 = multiprocessing.Process(
-        name='worker 2',
-        target=scam(),
-    )
-    worker_3 = multiprocessing.Process(
-        name='worker 3',
-        target=scam(),
-    )
-    worker_4 = multiprocessing.Process(
-        name='worker 4',
-        target=scam(),
-    )
-    worker_5 = multiprocessing.Process(
-        name='worker 5',
-        target=scam(),
-    )
-    worker_6 = multiprocessing.Process(
-        name='worker 6',
-        target=scam(),
-    )
-    worker_7 = multiprocessing.Process(
-        name='worker 7',
-        target=scam(),
-    )
-    worker_8 = multiprocessing.Process(
-        name='worker 8',
-        target=scam(),
-    )
-    worker_9 = multiprocessing.Process(
-        name='worker 9',
-        target=scam(),
-    )
-    worker_10 = multiprocessing.Process(
-        name='worker 10',
-        target=scam(),
-    )
-    worker_11 = multiprocessing.Process(
-        name='worker 11',
-        target=scam(),
-    )
-    worker_12 = multiprocessing.Process(
-        name='worker 12',
-        target=scam(),
-    )
-    worker_13 = multiprocessing.Process(
-        name='worker 13',
-        target=scam(),
-    )
-    worker_14 = multiprocessing.Process(
-        name='worker 14',
-        target=scam(),
-    )
-    worker_15 = multiprocessing.Process(
-        name='worker 15',
-        target=scam(),
-    )
-    worker_16 = multiprocessing.Process(
-        name='worker 16',
-        target=scam(),
-    )
-    worker_17 = multiprocessing.Process(
-        name='worker 17',
-        target=scam(),
-    )
-    worker_18 = multiprocessing.Process(
-        name='worker 18',
-        target=scam(),
-    )
-    worker_19 = multiprocessing.Process(
-        name='worker 19',
-        target=scam(),
-    )
-    worker_20 = multiprocessing.Process(
-        name='worker 20',
-        target=scam(),
-    )
-    worker_21 = multiprocessing.Process(
-        name='worker 21',
-        target=scam(),
-    )
-    worker_22 = multiprocessing.Process(
-        name='worker 22',
-        target=scam(),
-    )
-    worker_23 = multiprocessing.Process(
-        name='worker 23',
-        target=scam(),
-    )
-    worker_24 = multiprocessing.Process(
-        name='worker 24',
-        target=scam(),
-    )
-    worker_25 = multiprocessing.Process(
-        name='worker 25',
-        target=scam(),
-    )
-    worker_26 = multiprocessing.Process(
-        name='worker 26',
-        target=scam(),
-    )
-    worker_27 = multiprocessing.Process(
-        name='worker 27',
-        target=scam(),
-    )
-
-    worker_0.start()
-    worker_1.start()
-    worker_2.start()
-    worker_3.start()
-    worker_4.start()
-    worker_5.start()
-    worker_6.start()
-    worker_7.start()
-    worker_8.start()
-    worker_9.start()
-    worker_10.start()
-    worker_11.start()
-    worker_12.start()
-    worker_13.start()
-    worker_14.start()
-    worker_15.start()
-    worker_16.start()
-    worker_17.start()
-    worker_18.start()
-    worker_19.start()
-    worker_20.start()
-    worker_21.start()
-    worker_22.start()
-    worker_23.start()
-    worker_24.start()
-    worker_25.start()
-    worker_26.start()
-    worker_27.start()
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    while True:
+        gen()
